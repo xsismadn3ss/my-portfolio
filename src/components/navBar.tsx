6a3d5c386d974dbad7771750f91933674d10a9d9
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-06-04 23:10:21 -0600

    añadir hover card para perfil de github y encapsular el titulo del navbar para que active el evento para mostrar la tarjeta

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Link from "next/link"
 import SwitchTheme from "./switchTheme"
+import { GithubHoverCard } from "./githubProfile"
 
 const routes = [
   { title: 'Inicio', path: '/' },
@@ -11,12 +12,13 @@ const routes = [
 
 
 export default function NavBar(): React.ReactElement {
-
   return (
-    <header className="px-6 py-2 border-b-[2px] backdrop-blur-lg fixed top-0 w-full">
+    <header className="px-6 py-2 backdrop-blur-lg fixed top-0 w-full">
       <div className="flex justify-between items-end">
         <nav className="flex">
-          <h1 className="font-bold text-2xl mr-6 drop-shadow-md">xsismadn3ss</h1>
+          <GithubHoverCard username="xsismadn3ss">
+            <h1 className="font-bold text-2xl mr-4 drop-shadow-md">xsismadn3ss</h1>
+          </GithubHoverCard>
           <ul className="flex gap-2 self-end">
             {routes.map((route, index) => (
               <li key={index}>
