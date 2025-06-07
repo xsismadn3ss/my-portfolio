2c0cf7846109b9df6ee1580d50899c1cbbeb4bec
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-06-07 01:29:57 -0600

    añadir pagina para /projects/api_rest

---
+++ src/app/projects/api_rest/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "API REST",
+    description: "Ejemplo de una API REST en Next.js"
+}
+
+export default function ApiRestPage() {
+    return (<></>)
+}
