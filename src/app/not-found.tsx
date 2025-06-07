505b6337ad1b83c24508bf4b82cff5abf737ff0a
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-06-06 22:30:36 -0600

    actualizar estilos

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,9 +8,10 @@ export default function NotFound(): React.ReactNode {
     <>
       <section className="flex justify-center h-[50vh] items-center">
         <article className="flex flex-col items-center">
-          <h1 className="font-bold text-[4rem]
-          bg-gradient-to-r from-emerald-700 via-cyan-700 to-primary/80 text-transparent bg-clip-text
-          ">404</h1>
+          <div className="flex gap-4 items-center">
+          <Ghost className="scale-200 text-emerald-800" />
+          <h1 className="font-bold text-[4rem] bg-gradient-to-r from-emerald-700 via-cyan-700 to-primary/80 text-transparent bg-clip-text">404</h1>
+          </div>
           <p className="mb-4 text-muted-foreground">La pagina que estas buscando no existe</p>
           <Button>
             <Link href="/" className="text-lg accent-accent">Volver al inicio</Link>
