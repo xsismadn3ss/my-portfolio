a97a4a2d081dd483f61d5076f1560c27b21d872c
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-06-06 20:33:46 -0600

    actualizar estilos de la barra de navegacion

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -13,11 +13,13 @@ const routes = [
 
 export default function NavBar(): React.ReactElement {
   return (
-    <header className="px-6 py-2 backdrop-blur-lg fixed top-0 w-full">
+    <header className="px-6 py-2 bg-transparent backdrop-blur-lg fixed top-0 w-full border-[2px]">
       <div className="flex justify-between items-end">
         <nav className="flex">
           <GithubHoverCard username="xsismadn3ss">
-            <h1 className="font-bold text-2xl mr-4 drop-shadow-md">xsismadn3ss</h1>
+            <h1 className="font-bold text-2xl mr-4 drop-shadow-md  hover:bg-gradient-to-r hover:bg-clip-text hover:text-transparent hover:from-emerald-500 hover:via-cyan-600 hover:to-primary/80
+            dark:hover:from-emerald-300 dark:hover:via-cyand-300
+            transition-colors duration-500 ease-in">xsismadn3ss</h1>
           </GithubHoverCard>
           <ul className="flex gap-2 self-end">
             {routes.map((route, index) => (
