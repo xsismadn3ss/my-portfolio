cf1c7c9528ac8a159b5beb3b2199af82f9739e74
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-06-07 22:45:59 -0600

    crear metadata para la pagina, agregar componente de documentación de API para el proyecto, añadir imagen y titulo para la pagina

--- src/app/projects/api_rest/page.tsx
+++ src/app/projects/api_rest/page.tsx
@@ -1,10 +1,46 @@
-import type { Metadata } from "next"
+import type { Metadata } from "next";
+import { projects } from "../page";
+import ApiDocumentation from "./apiDocumentation";
+import Image from "next/image";
 
-export const metadata: Metadata = {
-    title: "API REST",
-    description: "Ejemplo de una API REST en Next.js"
-}
+const project = projects.personal.find((p) => p.title == "API REST DEMO");
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: project?.title,
+    description: project?.description,
+    openGraph: {
+      title: project?.title,
+      description: project?.description,
+      url: `${process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"}${
+        project?.link
+      }`,
+      images: [
+        {
+          url: `${project?.image}`,
+          height: 500,
+          width: 500,
+          alt: project?.title,
+        },
+      ],
+    },
+  };
+};
 
 export default function ApiRestPage() {
-    return (<></>)
+  return (
+    <>
+      <div className="flex justify-center pt-10">
+      <Image
+      src={`${project?.image}`}
+      alt="project image"
+      width={220}
+      height={220}
+      className="rounded-md border-2"
+      />
+      </div>
+      <h1 className="text-3xl text-center"><span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-emerald-700 via-cyan-600 to-primary/80 w-fit">{project?.title}</span></h1>
+      <ApiDocumentation />
+    </>
+  );
 }
