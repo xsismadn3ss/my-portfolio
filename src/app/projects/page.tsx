45c11c049585fd82a38c7d798f0985902879047a
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-06-07 19:12:28 -0600

    crear galeria de listado de proyectos, modificar metadata

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,27 +1,124 @@
-import type { Metadata } from "next"
-import Link from "next/link"
+import type { Metadata } from "next";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
-export const metadata: Metadata = {
-  title: "Proyectos",
-  description: "Listado de proyectos web realizados, en esta página podrás encontrar proyectos de prectica o proyectos profesionales realizados en empresas."
+const projects = {
+  personal: [
+    {
+      title: "API REST DEMO",
+      description:
+        "Un ejemplo de una API REST construida con Next.js, junto con documentación de definición de la API.",
+      link: "/projects/api_rest",
+      image: "/projects/api_rest.png",
+    },
+  ],
+  proffessional: [],
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+  const url = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+
+  return {
+    title: "Proyectos",
+    description:
+      "Listado de proyectos web realizados, en esta página podrás encontrar proyectos de prectica o proyectos profesionales realizados en empresas.",
+    openGraph: {
+      title: "Proyectos",
+      description:
+        "Listado de proyectos web realizados, en esta página podrás encontrar proyectos de prectica o proyectos profesionales realizados en empresas.",
+      url: `${url}/projects`,
+      siteName: "Proyectos",
+      // images:[
+      //   {
+      //     url: `${url}/og/projects.png`,
+      //     width: 1200,
+      //     height: 630,
+      //     alt: "Proyectos",
+      //   }
+      // ]
+    },
+  };
 }
+
+const ProjectCard = ({
+  project,
+}: {
+  project: {
+    title: string;
+    description: string;
+    link: string;
+    image: string;
+  };
+}) => {
+  return (
+    <Card>
+      <CardContent>
+        <Image
+          className="rounded-lg"
+          src={project.image}
+          alt={project.title}
+          width={500}
+          height={300}
+        />
+        <CardTitle className="text-lg text-primary/80">
+          {project.title}
+        </CardTitle>
+        <CardDescription className="mb-2">
+          {project.description}
+        </CardDescription>
+        <Link href={project.link}>
+          <Button className="w-full">Ver más</Button>
+        </Link>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default function ProjectsPage(): React.ReactNode {
   return (
     <>
-      <h1 className="text-2xl font-bold text-transparent bg-gradient-to-r bg-clip-text from-emerald-500 via-cyan-500 to-primary/60 w-fit">Proyectos</h1>
+      <h1 className="text-2xl font-bold text-transparent bg-gradient-to-r bg-clip-text from-emerald-500 via-cyan-500 to-primary/60 w-fit">
+        Proyectos
+      </h1>
       <section>
+        <article>
+          <h3 className="text-muted-foreground font-semibold text-xl">
+            Proyectos Profesionales
+          </h3>
+          {projects.proffessional.length === 0 ? (
+            <p className="text-primary/80">Aun no hay proyectos agregados</p>
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 my-4">
+              {projects.proffessional.map((project, index) => (
+                <ProjectCard key={index} project={project} />
+              ))}
+            </div>
+          )}
+          <hr className="mt-2" />
+        </article>
         <article className="mt-2">
-          <h3 className="text-muted-foreground font-semibold">Proyectos personales</h3>
-          <ul className="list-disc ml-6">
-            <li className="font-semibold text-primary/85">
-              <Link href={'/projects/api_rest'} className="hover:underline">API REST Demo</Link>
-            </li>
-          </ul>
-
+          <h3 className="text-muted-foreground font-semibold text-xl">
+            Proyectos personales
+          </h3>
+          {projects.personal.length === 0 ? (
+            <p className="text-primary/80">Aun no hay proyectos agregados</p>
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 my-4">
+              {projects.personal.map((project, index) => (
+                <ProjectCard key={index} project={project} />
+              ))}
+            </div>
+          )}
         </article>
         <hr className="mt-2" />
       </section>
     </>
-  )
+  );
 }
