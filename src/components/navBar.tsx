1e8d215d93fab5acd2a7dae07cb43b10c39b61e5
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-06-07 22:43:00 -0600

    actualizar estilo de la barra de navegación para que los iconos cambien de escala

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,8 +1,8 @@
-'use client'
-import Link from "next/link"
-import SwitchTheme from "./switchTheme"
-import { GithubHoverCard } from "./githubProfile"
-import { Equal, GithubIcon } from "lucide-react"
+"use client";
+import Link from "next/link";
+import SwitchTheme from "./switchTheme";
+import { GithubHoverCard } from "./githubProfile";
+import { Equal, GithubIcon } from "lucide-react";
 import {
   Drawer,
   DrawerClose,
@@ -10,16 +10,16 @@ import {
   DrawerDescription,
   DrawerHeader,
   DrawerTitle,
-  DrawerTrigger
-} from "./ui/drawer"
-import { usePathname } from "next/navigation"
+  DrawerTrigger,
+} from "./ui/drawer";
+import { usePathname } from "next/navigation";
 
 const routes = [
-  { title: 'Inicio', path: '/' },
-  { title: 'Sobre mi', path: '/about' },
-  { title: 'Proyectos', path: '/projects' },
-  { title: 'Contacto', path: '/contact' }
-]
+  { title: "Inicio", path: "/" },
+  { title: "Sobre mi", path: "/about" },
+  { title: "Proyectos", path: "/projects" },
+  { title: "Contacto", path: "/contact" },
+];
 
 function DrawerMenu() {
   return (
@@ -29,42 +29,41 @@ function DrawerMenu() {
       </DrawerTrigger>
       <DrawerContent className="dark:bg-transparent dark:backdrop-blur-2xl">
         <DrawerHeader>
-          <DrawerTitle>
-            Menu
-          </DrawerTitle>
-          <DrawerDescription>
-          </DrawerDescription>
+          <DrawerTitle>Menu</DrawerTitle>
+          <DrawerDescription></DrawerDescription>
         </DrawerHeader>
         <ul className="flex flex-col gap-4 px-4 pb-6">
           {routes.map((route, index) => (
             <li key={index}>
-              <DrawerClose>
-                <Link href={route.path}>{route.title}</Link>
-              </DrawerClose>
+              <Link href={route.path}>
+                <DrawerClose>{route.title}</DrawerClose>
+              </Link>
             </li>
           ))}
         </ul>
       </DrawerContent>
     </Drawer>
-  )
+  );
 }
 
-
 export default function NavBar(): React.ReactElement {
-
-  const currentRoute = usePathname()
-  const selected = routes.find((route) => route.path === currentRoute)?.title
+  const currentRoute = usePathname();
+  const selected = routes.find((route) => route.path === currentRoute)?.title;
 
   return (
     <div className="flex justify-center">
-      <header className="px-4 py-2 bg-transparent backdrop-blur-lg fixed top-0 w-full lg:w-5/6 border-l-[2px] border-b-[2px] border-r-[2px] rounded-bl-md rounded-br-md z-50">
+      <header className="px-4 py-2 bg-transparent backdrop-blur-lg fixed top-0 w-full lg:w-[140vh] border-l-[2px] border-b-[2px] border-r-[2px] rounded-bl-md rounded-br-md z-50">
         <div className="flex justify-between items-end pb-2">
           <nav className="flex items-end">
-            <Link href={'/'}>
-              <h1 className="font-bold text-2xl mr-4 drop-shadow-md  
+            <Link href={"/"}>
+              <h1
+                className="font-bold text-2xl mr-4 drop-shadow-md  
             hover:bg-gradient-to-r hover:bg-clip-text hover:text-transparent hover:from-emerald-500 hover:via-cyan-600 hover:to-primary/80
             dark:hover:from-emerald-300 dark:hover:via-cyand-300
-            hover:transition-colors hover:duration-500 duration-100 hover:ease-in">xsismadn3ss</h1>
+            hover:transition-colors hover:duration-500 duration-100 hover:ease-in scale-80 sm:scale-100"
+              >
+                xsismadn3ss
+              </h1>
             </Link>
             <ul className=" hidden md:flex gap-2">
               {routes.map((route, index) => (
@@ -73,20 +72,26 @@ export default function NavBar(): React.ReactElement {
                     href={route.path}
                     className={`px-2 py-1 font-semibold bg-inherit
                       hover:bg-primary/20 transition duration-200 ease-linear rounded-md
-                    ${selected === route.title
-                        ? 'bg-gradient-to-r from-emerald-600 via-cyan-600 to-primary/80 dark:text-primary text-accent'
-                        : 'bg-inherit'
-                      }`}>{route.title}</Link>
+                    ${
+                      selected === route.title
+                        ? "bg-gradient-to-r from-emerald-600 via-cyan-600 to-primary/80 dark:text-primary text-accent"
+                        : "bg-inherit"
+                    }`}
+                  >
+                    {route.title}
+                  </Link>
                 </li>
               ))}
             </ul>
           </nav>
-          <div className="flex gap-2 items-end">
+          <div className="flex gap-2 items-end scale-75 md:scale-100">
             <SwitchTheme />
             <GithubHoverCard username="xsismadn3ss" className="pt-6">
-              <div className="bg-primary/75 dark:bg-primary/90 rounded-full scale-125 p-[4px] transition-colors duration-150 ease-in
+              <div
+                className="bg-primary/75 dark:bg-primary/90 rounded-full scale-125 p-[4px] transition-colors duration-150 ease-in
               hover:bg-gradient-to-b hover:from-emerald-500 hover:via-cyan-600 hover:to-primary/80
-            dark:hover:from-emerald-300 dark:hover:via-cyand-300 dark:hover:to-sky-300 hover:duration-500 hover:ease-in-out">
+            dark:hover:from-emerald-300 dark:hover:via-cyand-300 dark:hover:to-sky-300 hover:duration-500 hover:ease-in-out"
+              >
                 <GithubIcon className="text-white dark:text-gray-900 transition-colors duration-100 ease-in" />
               </div>
             </GithubHoverCard>
@@ -95,5 +100,5 @@ export default function NavBar(): React.ReactElement {
         </div>
       </header>
     </div>
-  )
+  );
 }
