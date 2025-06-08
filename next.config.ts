9fc1f16ad15450ac8cb8789100deac82227f7244
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-06-07 23:49:27 -0600

    añadir configuración para sitios remotos en los que obtener las imagenes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      new URL(`${siteUrl}/assets/**`),
+      new URL(`${siteUrl}/assets/projects/**`),
+    ],
+  },
 };
 
 export default nextConfig;
